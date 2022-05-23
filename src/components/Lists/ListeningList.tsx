25ab48fccb0e9e692761a9db5d43cf685253a2a1
Moises Junior <m.junior@eq.ufrj.br>
2022-05-23 16:04:54 -0300

    feat: deletion warnings

--- src/components/Lists/ListeningList.tsx
+++ src/components/Lists/ListeningList.tsx
@@ -4,47 +4,75 @@ import useAuth from '../../hooks/useAuth'
 import { api } from '../../services/api'
 import { styles } from '../GlobalStyles'
 import { List } from '../MainPage'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 interface ListProps {
    albums: List[] | undefined
 }
 
-function ListeningList({ albums }: ListProps) { 
+function ListeningList({ albums }: ListProps) {
    const { auth, setReloadHome, reloadHome } = useAuth()
 
    function deleteAlbum(albumId: string) {
-      api.deleteUserAlbum(auth, albumId).then(() => {
-         setReloadHome(!reloadHome)
-      })
+      api.deleteUserAlbum(auth, albumId)
+         .then(() => {
+            setReloadHome(!reloadHome)
+            toast.success('Album deleted!', {
+               position: 'top-right',
+               autoClose: 1800,
+               hideProgressBar: false,
+               closeOnClick: true,
+               pauseOnHover: false,
+               draggable: false,
+               progress: undefined,
+               theme: 'dark'
+            })
+         })
+         .catch(e => {
+            toast.error(`${e.response.data}`, {
+               position: 'top-right',
+               autoClose: 1800,
+               hideProgressBar: false,
+               closeOnClick: true,
+               pauseOnHover: false,
+               draggable: false,
+               progress: undefined,
+               theme: 'dark'
+            })
+         })
    }
 
    return (
-      <Box sx={styles.listContainer}>
-         {albums?.map(a => (
-            <Card sx={styles.cardAlbum} key={a.albumId}>
-               <img width='80px' src={a.album.cover} alt='album info' />
-               <CardContent>
-                  <Typography sx={styles.cardAlbumName}>
-                     {a.album.name}
-                  </Typography>
-                  <Typography sx={styles.cardArtistName}>
-                     {a.album.artist}
-                  </Typography>
-               </CardContent>
-               <DeleteIcon
-                  sx={{
-                     fontSize: '20px',
-                     position: 'absolute',
-                     top: '5px',
-                     right: '5px',
-                     color: '#1DB954',
-                     cursor: 'pointer'
-                  }}
-                  onClick={() => deleteAlbum(a.albumId)}
-               />
-            </Card>
-         ))}
-      </Box>
+      <>
+         <Box sx={styles.listContainer}>
+            {albums?.map(a => (
+               <Card sx={styles.cardAlbum} key={a.albumId}>
+                  <img width='80px' src={a.album.cover} alt='album info' />
+                  <CardContent>
+                     <Typography sx={styles.cardAlbumName}>
+                        {a.album.name}
+                     </Typography>
+                     <Typography sx={styles.cardArtistName}>
+                        {a.album.artist}
+                     </Typography>
+                  </CardContent>
+                  <DeleteIcon
+                     sx={{
+                        fontSize: '20px',
+                        position: 'absolute',
+                        top: '5px',
+                        right: '5px',
+                        color: '#1DB954',
+                        cursor: 'pointer'
+                     }}
+                     onClick={() => deleteAlbum(a.albumId)}
+                  />
+               </Card>
+            ))}
+         </Box>
+         <ToastContainer />
+      </>
    )
 }
 
